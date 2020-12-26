c90452f14dac2034d8d8c8cc4087d9c13fe7bb79
Tereza Gabrielova <tgabriel@redhat.com>
2020-12-26 19:34:54 +0100

    Delete not used directories

--- functional-tests.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-export SIMPLETEST_DB="mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}/${MYSQL_DATABASE}"
-export SIMPLETEST_BASE_URL="http://drupal-test-1:8080"
-cd /app/web/core && ../../vendor/bin/phpunit tests/Drupal/FunctionalTests/Datetime/TimestampTest.php
